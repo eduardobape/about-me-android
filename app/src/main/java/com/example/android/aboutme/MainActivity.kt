d2312773dd2dc829c2c4eb25626cd1039283d942
eduardobape <3110718+eduardobape@users.noreply.github.com>
2021-12-14 02:33:11 +0100

    Add a button to show the nickname

--- app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -1,13 +1,32 @@
 package com.example.android.aboutme
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
 import com.example.android.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.bShowNickname.setOnClickListener {
+            showNickName()
+        }
+    }
+
+    private fun showNickName() {
+        binding.etNickname.visibility = View.GONE
+        binding.bShowNickname.visibility = View.GONE
+        binding.tvNickname.text = binding.etNickname.text.toString()
+        binding.tvNickname.visibility = View.VISIBLE
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(binding.root.windowToken, 0)
     }
 }
