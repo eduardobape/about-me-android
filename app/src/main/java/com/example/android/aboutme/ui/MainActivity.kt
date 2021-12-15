9e1e8701208c49f4d8478f1a3856bec9f62cc479
eduardobape <3110718+eduardobape@users.noreply.github.com>
2021-12-15 19:15:16 +0100

    Bind a user to the layout of MainActivity
    Also, use binding to show the user's nickname in this layout

--- app/src/main/java/com/example/android/aboutme/ui/MainActivity.kt
+++ app/src/main/java/com/example/android/aboutme/ui/MainActivity.kt
@@ -7,26 +7,31 @@ import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.android.aboutme.R
+import com.example.android.aboutme.data.User
 import com.example.android.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var myUser: User? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        myUser = User("Pepe")
+        binding.user = myUser
         binding.bShowNickname.setOnClickListener {
             showNickName()
         }
     }
 
     private fun showNickName() {
+        myUser?.nickname = binding.etNickname.text.toString()
         binding.apply {
+            invalidateAll()
             etNickname.visibility = View.GONE
             bShowNickname.visibility = View.GONE
-            tvNickname.text = binding.etNickname.text.toString()
             tvNickname.visibility = View.VISIBLE
         }
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
